363a3985f51c041a82c2bff90294cc0fbc1cc60d
Ivan Escobar <ivnescbr1989@gmail.com>
2025-02-13 16:40:11 -0500

    finish project

--- 07-unsplash-images/starter/src/context.jsx
+++ 07-unsplash-images/starter/src/context.jsx
@@ -1,18 +1,31 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 
 const AppContext = createContext()
-
+const getInitialDarkMode = () => {
+  const prefersDark = window.matchMedia('(prefers-color-scheme:dark)').matches
+  const storedMode = localStorage.getItem('dark-theme')
+  if(storedMode === null) return prefersDark
+  return storedMode === 'true'
+}
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false)
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode())
+  const [searchTerm, setSearchTerm] = useState('cat')
 
   const toggleTheme = () => {
     const newTheme = !isDarkTheme
     setIsDarkTheme(newTheme)
-    const body = document.querySelector('body')
-    body.classList.toggle('dark-theme', newTheme)
-    console.log(body);
+    localStorage.setItem('dark-theme', newTheme)
   }
+  useEffect(()=>{
+    document.body.classList.toggle('dark-theme', isDarkTheme)
+  }, [isDarkTheme])
 
-  return <AppContext.Provider value={{toggleTheme, isDarkTheme}}>{children}</AppContext.Provider>
+  return (
+    <AppContext.Provider
+      value={{ toggleTheme, isDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
 }
 export const useGlobalContext = () => useContext(AppContext)
--- 07-unsplash-images/starter/src/gallery.jsx
+++ 07-unsplash-images/starter/src/gallery.jsx
@@ -1,6 +1,56 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { useGlobalContext } from './context'
+
+const url =
+  `https://api.unsplash.com/search/photos?client_id=${import.meta.env.VITE_API_KEY}`
 const Gallery = () => {
+  const { searchTerm } = useGlobalContext()
+  const response = useQuery({
+    queryKey: ['images', searchTerm],
+    queryFn: async () => {
+      const res = await axios(`${url}&query=${searchTerm}`)
+      return res.data
+    },
+  })
+
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    )
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>There seems to be an error...</h4>
+      </section>
+    )
+  }
+  const results = response.data.results
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No reults found... Try a different search term</h4>
+      </section>
+    )
+  }
+
   return (
-    <div>Gallery</div>
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          />
+        )
+      })}
+    </section>
   )
 }
 export default Gallery
